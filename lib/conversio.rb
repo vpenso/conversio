5ee6247bb29753f050c470b66822e102fd805d3b
Victor Penso <v.penso@gsi.de>
2010-11-19 12:54:29 +0100

    bigging to implement support for multiple templates

--- lib/conversio.rb
+++ lib/conversio.rb
@@ -1,3 +1,19 @@
 require 'conversio/converter'
 require 'conversio/pygmentizer'
 require 'conversio/htmltoc'
+
+module Conversio
+  class Template
+  end
+end
+
+# find the template directory
+templates = File.join(File.dirname(File.expand_path(__FILE__)),'..','templates')
+# find the ERB templates 
+Dir.glob("#{templates}/*.erb") do |template|
+  # add the template as constant to the class Templates
+  Conversio::Template.const_set(
+    File.basename(template,".erb").upcase, 
+    File.read(template))
+end
+
